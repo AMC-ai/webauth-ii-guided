c7bd0d181b356ebb7dafe782dcaeeb1380e0ed06
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-12-17 13:47:04 -0500

    added session store and dep.

--- api/server.js
+++ api/server.js
@@ -2,18 +2,32 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const sessions = require('express-session'); //install express-session
+const KnexSessionStore = require("connect-session-knex")(sessions); //to store in db
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+const knex = require('../database/dbConfig');
 
 const server = express();
 
 const sessionConfiguration = {
+
   //session storage options
   name: 'chocolatechip', //default would be sid
   secret: 'keep it secret, keep it safe!', //used for encryption (must be an env var)
   saveUninitialized: true, //has implications w/ 
   resave: false, //read more for this later
+
+  //how to store sessions
+  store: new KnexSessionStore({
+    //do not forget new (keyword) bc you get back a constructor function
+    knex,
+    tablename: "sessions",
+    createtable: true,
+    sidfieldname: "sid",
+    clearInterval: 1000 * 60 * 10,
+  }),
+
   //cookie options
   cookie: {
     maxAge: 1000 * 60 * 10, // 10 mins in milliseconds
